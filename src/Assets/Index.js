0a21e354c4af83ea003f194e24961c02b77aac35
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-28 03:51:22 +0100

    Cooking

---
+++ src/Assets/Index.js
@@ -0,0 +1,13 @@
+import LinkIcon from './link.svg'
+import Loader from './loader.svg'
+import Copy from './copy.svg'
+import Logo from './logo.png'
+import Tick from './tick.svg'
+
+export {
+    LinkIcon,
+    Loader,
+    Copy,
+    Logo,
+    Tick
+}
