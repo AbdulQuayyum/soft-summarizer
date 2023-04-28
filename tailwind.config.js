fbf2e80396e0e60c882d5fa91d181979a279f68d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-28 04:06:34 +0100

    Cooking

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ['Satoshi', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
+        nunito: ['Nunito', 'sans-serif'],
       }
     },
   },
