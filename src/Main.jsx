552e37343c5b2e822ec62d7835170f006006f92d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-29 03:04:55 +0100

    Cooking...

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
 
+import { Background } from "./Theme/Index.jsx"
 import { Store } from './Services/Store.js';
+import { ThemeProvider } from './Contexts/ThemeContext.jsx';
 import App from './App.jsx'
 import "./Styles/Index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <Background>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Background>
     </Provider>
   </React.StrictMode>,
 )
