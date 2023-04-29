bba5f0c7f767d2aa20686510059762d5ffd64eae
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-29 04:16:59 +0100

    Another project in the bag...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { AnalyticsWrapper } from "./Components/Analytics";
 import MainLayout from "./Layout/Main.Layout"
 
 function App() {
 
   return (
     <>
+      <AnalyticsWrapper />
       <MainLayout />
     </>
   )
