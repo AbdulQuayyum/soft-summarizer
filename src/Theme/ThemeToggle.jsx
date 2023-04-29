ba42e7f4698046b2f22d99cfac7832212e97d8b1
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-04-29 03:00:46 +0100

    Cooking...

---
+++ src/Theme/ThemeToggle.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaSun, FaMoon } from "react-icons/fa"
+import { ThemeContext } from '../Contexts/ThemeContext'
+
+const Toggler = () => {
+    const { theme, setTheme } = React.useContext(ThemeContext)
+
+    return (
+        <div className="transition duration-500 ease-in-out flex justify-center items-center">
+            {theme === 'dark' ? (
+                <FaSun
+                    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                    className="text-gray-500 dark:text-gray-50 transition-all duration-500 w-6 h-6 md:w-8 md:h-6 flex justify-center items-center cursor-pointer"
+                />
+            ) : (
+                <FaMoon
+                    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                    className="text-[#1c1c24] dark:text-gray-50 transition-all duration-500 w-6 h-6 md:w-8 md:h-6 flex justify-center items-center cursor-pointer"
+                />
+            )}
+        </div>
+    )
+}
+
+export default Toggler
